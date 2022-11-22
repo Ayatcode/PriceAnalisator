be6996a239ad51173ea57de94830deb7f2e2627d
Ayatcode <2mwkd2k@code.edu.az>
2022-11-22 19:59:37 +0400

    21.11.2022 Task

---
+++ 21.11.2022/Strbuilder/Strbuilder/Program.cs
@@ -0,0 +1,14 @@
+﻿BuilderString BuilderString=new BuilderString();
+
+BuilderString.Append('a');
+BuilderString.Append('b');
+BuilderString.Append('c');
+BuilderString.Append('d');
+BuilderString.Append('a');
+BuilderString.Append('f');
+BuilderString.Append('a');
+
+
+BuilderString.Remove(2,5);
+
+Console.WriteLine(BuilderString);
---
+++ 21.11.2022/Strbuilder/Strbuilder/strBuilder.cs
@@ -0,0 +1,81 @@
+﻿class BuilderString
+{
+    private char[] _arr;
+    public int Capacity { get; set; }
+    public BuilderString()
+    {
+        Capacity = 0;
+        _arr = new char[Capacity];
+    }
+
+    public BuilderString(int capacity)
+    {
+        Capacity = capacity;
+        _arr = new char[Capacity];
+    }
+
+    public void Append(char symbol)
+    {
+        if (Capacity == 0)
+        {
+            Capacity = 16;
+            Array.Resize(ref _arr, Capacity);
+        }
+        if (_arr[Capacity - 1] != 0)
+        {
+            Capacity *= 2;
+            Array.Resize(ref _arr, Capacity);
+            _arr[Capacity / 2] = symbol;
+            return;
+        }
+
+        for (int i = 0; i < Capacity; i++)
+        {
+            if (_arr[i] == 0)
+            {
+                _arr[i] = symbol;
+                break;
+            }
+
+        }
+    }
+
+    public BuilderString Replace(char oldVal, char newVal)
+    {
+        for (int i = 0; i < Capacity; i++)
+        {
+            if (_arr[i] == 0) break;
+            if (_arr[i] == oldVal)
+            {
+                _arr[i] = newVal;
+            }
+        }
+        return this;
+    }
+
+    public override string ToString()
+    {
+        string result = string.Empty;
+        foreach (char item in _arr)
+        {
+            if (item == 0) break;
+            result += item;
+        }
+        return result;
+    }
+
+    public void Append(string symbol)
+    {
+        for (int i = 0; i < symbol.Length; i++)
+        {
+            Append(symbol[i]);
+        }
+    }
+    public void Remove(int starindex,int lastindex)
+    {
+        for (int i = starindex; i < _arr.Length; i++)
+        {
+            _arr[i] =' ';
+        }
+    }
+}
