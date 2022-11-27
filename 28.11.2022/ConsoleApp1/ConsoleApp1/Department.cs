48e5e80450ea2aef95c9ed6d83ab947f8c44f7eb
Ayatcode <2mwkd2k@code.edu.az>
2022-11-28 01:06:46 +0400

    28.11.2022

---
+++ 28.11.2022/ConsoleApp1/ConsoleApp1/Department.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+        public int EmployeeLimit { get; set; }
+
+        public Employee[] Employeearray;
+        public Department()
+        {
+            Employeearray = new Employee[EmployeeLimit];
+
+        }
+
+        public Employee this[int index] 
+        { 
+            
+            get 
+            { 
+                if(index>= Employeearray.Length || index<0) 
+                { throw new CapacityLimitException("Limit");  }
+                return Employeearray[index]; 
+            } 
+            set
+            {
+                Employeearray[index] = value;
+            } 
+        }
+    }
+}
---
+++ 28.11.2022/ConsoleApp1/ConsoleApp1/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Employee : IPerson
+    {
+        public string Name { get; set ; }
+        public int Age { get ; set; }
+        public int Salary { get; set; }
+        public int id { get;  }
+        private static int _id;
+
+        public Employee(string name,int age,int salary) 
+        { 
+            Name = name;
+            Age = age;
+            Salary = salary;
+            _id++;
+            id = _id;
+        }
+
+        public string Showinfo()
+        {
+            return $" ID:{id},Name:{Name},Age{Age},Salary:{Salary}";
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
