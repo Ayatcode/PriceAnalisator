144f666cca6d8eedb91d48dc17f3cc4949f997c9
Ayatcode <2mwkd2k@code.edu.az>
2022-11-29 21:35:11 +0400

    Delegate task

---
+++ 29.11.2022/Delegate/delegate/FIndall.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Fidndall<T>
+{
+    private T[] _arr; 
+
+
+    public void New_array(T []arr, Check Func)
+    {
+        _arr= new T[arr.Length];
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (Func(arr[i]))
+            {
+                _arr[i] = arr[i];
+            }
+
+
+        }
+        foreach (var item in _arr)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+    public delegate bool Check(T arr);
+
+
+}
+
---
+++ 29.11.2022/Delegate/delegate/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int[] arr = { 1, 2, 3, 4 };
+
+bool IsPositive(int value)=> value > 0;
+
+
+Fidndall<int> fidndall= new Fidndall<int>();
+
+
+fidndall.New_array(arr, IsPositive);
