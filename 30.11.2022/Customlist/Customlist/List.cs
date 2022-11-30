65a9a97e28e596050918f80c4f39b9accc9c5b26
Ayatcode <2mwkd2k@code.edu.az>
2022-11-30 21:26:03 +0400

    Custom list

---
+++ 30.11.2022/Customlist/Customlist/List.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Customlist
+{
+    public class List<T>
+    {
+        public T[] arr;
+        private static int count;
+        public int Capacity { get; set; }
+        public int Count { get => count; }
+
+        public List()
+        {
+            Capacity = 0;
+        }
+
+        public List(int capacity)
+        {
+            Capacity = capacity;
+        }
+
+        public void Add(T item)
+        {
+            if (Capacity == 0)
+            {
+                Capacity = 2;
+                Array.Resize(ref arr, Capacity);
+                arr[0] = item;
+                count++;
+                return;
+            }
+            if (Count == Capacity)
+            {
+                Capacity = Capacity * 2;
+                Array.Resize(ref arr, Capacity);
+                arr[Capacity / 2] = item;
+                count++;
+                return;
+            }
+            arr[count] = item;
+            count++;
+        }
+        public bool Exist(Predicate<T> macth)
+        {
+            foreach (var item in arr)
+            {
+                if (macth(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ 30.11.2022/Customlist/Customlist/Program.cs
@@ -0,0 +1,16 @@
+﻿List<int> item = new();
+
+
+item.Add(0);
+item.Add(1);
+item.Add(2);
+item.Add(3);
+item.Add(4);
+item.Add(5);    
+item.Add(6);
+
+foreach (var num in item)
+{
+    Console.WriteLine(num);
+}
+ 
