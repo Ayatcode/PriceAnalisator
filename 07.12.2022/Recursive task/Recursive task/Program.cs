e34bf401c7bcb9a5e46384f1866930f2d3e33f73
Ayatcode <2mwkd2k@code.edu.az>
2022-12-07 14:51:44 +0400

    Recursive Directories

---
+++ 07.12.2022/Recursive task/Recursive task/Program.cs
@@ -0,0 +1,20 @@
+﻿void  Func(DirectoryInfo folder)
+{
+    List<string> res=new();
+   if(folder.GetDirectories().Length>0)
+    {
+        foreach (var item in folder.GetDirectories())
+        {
+            Console.WriteLine(item);
+            Func(item);
+        }
+        
+    }
+   
+    
+}
+
+
+DirectoryInfo res = new(@"C:\\Users\\User\\Desktop\\check");
+
+Func(res);
