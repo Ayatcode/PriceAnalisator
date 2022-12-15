044a8a9153909f5f5ebe9bb2f92ebdf1f0d8c9ad
Ayatcode <2mwkd2k@code.edu.az>
2022-12-15 21:42:27 +0400

    Ado.net

---
+++ 15.12.2022/Adonet/Adonet/NotFoundExpection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adonet
+{
+    public class NotFoundExpection:Exception
+    {
+        public NotFoundExpection(string message) :base(message) 
+        {
+        }
+
+    }
+}
---
+++ 15.12.2022/Adonet/Adonet/Program.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.Data.SqlClient;
+
+using Adonet;
+using System.Drawing;
+
+string connectionString = @"Server=DESKTOP-SOO30PD\SQLEXPRESS;Database=BB101;Trusted_Connection=True";
+
+
+
+GetAllEmployees();
+
+List<Employee> GetAllEmployees()
+{
+    List<Employee> employees = new List<Employee>();
+    using (SqlConnection connection = new(connectionString))
+    {
+        connection.Open();
+
+        string query = "SELECT * FROM Empoloyee";
+
+        SqlCommand command = new(query, connection);
+        SqlDataReader reader = command.ExecuteReader();
+
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                Employee employee = new()
+                {
+                    ID = (int)reader["ID"],
+                    Name = (string)reader["Name"],
+                    Surname = (string)reader["Surname"],
+                    Salary = (decimal)reader["Salary"]
+
+                };
+                employees.Add(employee);
+                
+            }
+        }
+        return employees;
+    }
+}
+
+void GetEmployeeById(int id)
+{
+
+    using (SqlConnection connection = new(connectionString))
+    {
+        connection.Open();
+
+        string query = $"SELECT * FROM Employee WHERE Empolyee='@id'";
+       
+
+        SqlCommand command = new(query, connection);
+        command.Parameters.AddWithValue("@id", id);
+        SqlDataReader reader = command.ExecuteReader();
+        if(reader.HasRows)
+        {
+
+        while(reader.Read())
+        {
+            Employee employee = new()
+            {
+                ID = (int)reader["ID"],
+                Name = (string)reader["Name"],
+                Surname = (string)reader["Surname"],
+                Salary = (decimal)reader["Salary"]
+
+            };
+        }
+        Console.WriteLine(reader["ID"]);
+        }
+        else
+        {
+            throw new NotFoundExpection("Not found");
+        }
+
+    }
+}
+
+void CreateEmployee(Employee employee)
+{
+    using (SqlConnection connection = new(connectionString))
+    {
+        connection.Open();
+
+        string query = "INSERT INTO Groups VALUES ('@name','@Surname','@Salary')";
+
+        SqlCommand command = new(query, connection);
+        command.Parameters.AddWithValue("@name", employee.Name);
+        command.Parameters.AddWithValue("@Surname", employee.Surname);
+        command.Parameters.AddWithValue("@Salary", employee.Salary);
+        int result = command.ExecuteNonQuery(); 
+
+        Console.WriteLine($"{result} rows affected");
+    }
+}
+List<Employee> SearchSizesByName(string name)
+{
+    List<Employee> employees = new List<Employee>();
+    using (SqlConnection connection = new(connectionString))
+    {
+        connection.Open();
+
+        string query = $"SELECT * FROM Employee WHERE Employee LIKE '%' + @name + '%'";
+        
+
+        SqlCommand command = new(query, connection);
+        command.Parameters.AddWithValue("@name", name);
+        SqlDataReader reader = command.ExecuteReader();
+
+        if (reader.HasRows)
+        {
+            while (reader.Read())
+            {
+                Employee employee = new()
+                {
+                    ID = (int)reader["Id"],
+                    Name = (string)reader["Name"],
+                    Surname = (string)reader["Surname"],
+                    Salary = (decimal)reader["Salary"]
+
+                };
+                employees.Add(employee);
+                
+            }
+        }
+        return employees;
+    }
+}
