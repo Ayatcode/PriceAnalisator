13a48c94a21c602158d807c60918c4bbd3a10987
Ayatcode <2mwkd2k@code.edu.az>
2022-12-16 20:29:59 +0400

    Entity task

---
+++ 16.12.2022/Entity first/Entity first/Core/NotFounException/NotFoundExpection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_first.Core.NotFounException
+{
+    public class NotFoundExpection:Exception
+    {
+        public NotFoundExpection(string message ) : base(message) { } 
+    }
+}
---
+++ 16.12.2022/Entity first/Entity first/Entity/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Entity_first.Core.Classes;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity_first.Entity
+{
+    public class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-SOO30PD;Database=CodeFirstDb;Trusted_Connection=True;");
+        }
+
+        public DbSet<Employee> employees { get; set; }
+    }
+}
